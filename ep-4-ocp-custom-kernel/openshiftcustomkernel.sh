791c94c13efd257d20d08d48d654171bfec898cf
Rajiv Ranjan <mr.rajiv.ranjan@outlook.com>
2021-05-27 15:32:23 +0530

    Corrected gaps identified in shellcheck. Added the correct section options in the service unit file.

--- ep-4-ocp-custom-kernel/openshiftcustomkernel.sh
+++ ep-4-ocp-custom-kernel/openshiftcustomkernel.sh
@@ -6,7 +6,7 @@ is_reboot_required=0
 ## Current OSTree
 echo ""
 echo ""
-echo "############ Current OSTree ############"
+echo "###### Current OSTree ######"
 rpm-ostree status
 
 echo ""
@@ -15,18 +15,16 @@ echo "###### Target kernel details"
 ## Check if the conf file is present and retrieve the variables
 if test -f /etc/openshiftcustomkerneld/openshiftcustomkernel.conf ; 
   then
-    . /etc/openshiftcustomkerneld/openshiftcustomkernel.conf
+    source /etc/openshiftcustomkerneld/openshiftcustomkernel.conf
 
-    kernel_version=`echo $kernel_version | sed 's/ *$//g'`
-    kernel_arch=`echo $kernel_arch | sed 's/ *$//g'`
+    kernel_version=$(echo "$kernel_version" | sed 's/ *$//g')
+    kernel_arch=$(echo "$kernel_arch" | sed 's/ *$//g')
     
     kernel=kernel-$kernel_version.$kernel_arch
     kernel_core=kernel-core-$kernel_version.$kernel_arch
     kernel_modules=kernel-modules-$kernel_version.$kernel_arch
     kernel_modules_extra=kernel-modules-extra-$kernel_version.$kernel_arch
-    #openshift_base_kernel=$openshift_base_kernel
 
-    #echo "base openshift kernel=$openshift_base_kernel"
     echo "target kernel_version=$kernel_version"
     echo "target kernel_arch=$kernel_arch"
     echo "target kernel package=$kernel"
@@ -47,8 +45,8 @@ if test -f /etc/openshiftcustomkerneld/openshiftcustomkernel.conf ;
     ## Check if the target kernel is already installed
     echo ""
     echo "###### Checking for target packages install state"
-    rpm -q $kernel_core
-    is_kernel_core_installed=$?
+    rpm -q "$kernel_core"
+    is_target_kernel_core_installed=$?
     echo ""
 
 
@@ -56,7 +54,7 @@ if test -f /etc/openshiftcustomkerneld/openshiftcustomkernel.conf ;
 
     kernel_version_parts=(${kernel_version//-/ })
 
-    if [ $is_kernel_core_installed -eq 0 ] ; 
+    if [ $is_target_kernel_core_installed -eq 0 ] ; 
       then
         echo ""
         echo "###### Target kernel already installed: $kernel_core"
@@ -67,26 +65,28 @@ if test -f /etc/openshiftcustomkerneld/openshiftcustomkernel.conf ;
         echo ""
         echo "###### Downloading target kernel core: $kernel_core"
         echo "curl $fedora_download_base_url/${kernel_version_parts[0]}/${kernel_version_parts[1]}/$kernel_arch/$kernel_core.rpm --output $temp_folder/$kernel_core.rpm"
-        curl -s $fedora_download_base_url/${kernel_version_parts[0]}/${kernel_version_parts[1]}/$kernel_arch/$kernel_core.rpm --output $temp_folder/$kernel_core.rpm
+        curl -s "$fedora_download_base_url"/"${kernel_version_parts[0]}"/"${kernel_version_parts[1]}"/"$kernel_arch"/"$kernel_core".rpm --output $temp_folder/"$kernel_core".rpm --retry 10 --retry-all-errors
         echo ""
         echo "###### Downloading target kernel modules: $kernel_modules"
         echo "curl $fedora_download_base_url/${kernel_version_parts[0]}/${kernel_version_parts[1]}/$kernel_arch/$kernel_modules.rpm --output $temp_folder/$kernel_modules.rpm"
-        curl -s $fedora_download_base_url/${kernel_version_parts[0]}/${kernel_version_parts[1]}/$kernel_arch/$kernel_modules.rpm --output $temp_folder/$kernel_modules.rpm
+        curl -s "$fedora_download_base_url"/"${kernel_version_parts[0]}"/"${kernel_version_parts[1]}"/"$kernel_arch"/"$kernel_modules".rpm --output $temp_folder/"$kernel_modules".rpm --retry 10 --retry-all-errors
         echo ""
         echo "###### Downloading target kernel modules extra: $kernel_modules_extra"
         echo "curl $fedora_download_base_url/${kernel_version_parts[0]}/${kernel_version_parts[1]}/$kernel_arch/$kernel_modules_extra.rpm --output $temp_folder/$kernel_modules_extra.rpm"
-        curl -s $fedora_download_base_url/${kernel_version_parts[0]}/${kernel_version_parts[1]}/$kernel_arch/$kernel_modules_extra.rpm --output $temp_folder/$kernel_modules_extra.rpm
+        curl -s "$fedora_download_base_url"/"${kernel_version_parts[0]}"/"${kernel_version_parts[1]}"/"$kernel_arch"/"$kernel_modules_extra".rpm --output $temp_folder/"$kernel_modules_extra".rpm --retry 10 --retry-all-errors
         echo ""
         echo "###### Downloading target kernel: $kernel"
         echo "curl $fedora_download_base_url/${kernel_version_parts[0]}/${kernel_version_parts[1]}/$kernel_arch/$kernel.rpm --output $temp_folder/$kernel.rpm"
-        curl -s $fedora_download_base_url/${kernel_version_parts[0]}/${kernel_version_parts[1]}/$kernel_arch/$kernel.rpm --output $temp_folder/$kernel.rpm
+        curl -s "$fedora_download_base_url"/"${kernel_version_parts[0]}"/"${kernel_version_parts[1]}"/"$kernel_arch"/"$kernel".rpm --output $temp_folder/"$kernel".rpm --retry 10 --retry-all-errors
         
         echo ""
         echo "###### Installing the target kernel"
-        rpm-ostree override replace $temp_folder/$kernel_core.rpm $temp_folder/$kernel_modules.rpm $temp_folder/$kernel_modules_extra.rpm $temp_folder/$kernel.rpm
-
-        [[ $? -eq 0 ]] && is_reboot_required=1 
+        rpm-ostree override replace "$temp_folder"/"$kernel_core".rpm "$temp_folder"/"$kernel_modules".rpm "$temp_folder"/"$kernel_modules_extra".rpm "$temp_folder"/"$kernel".rpm
+        target_ostree_override_exit_code=$?
         
+        if [[ "${target_ostree_override_exit_code}" -eq 0 ]]; then 
+          is_reboot_required=1 
+        fi
         ## Verify the checksum of the downloaded target kernel
 
         ## Check the rpm ostree 
@@ -101,7 +101,9 @@ if test -f /etc/openshiftcustomkerneld/openshiftcustomkernel.conf ;
         ## Reboot to make the new rpm ostree effective
         echo ""
         echo "###### Is reboot required?: $is_reboot_required"
-        [[ is_reboot_required -eq 1 ]] && systemctl reboot
+        if [[ "${is_reboot_required}" -eq 1 ]]; then
+          systemctl reboot
+        fi
 
     fi
   else  
